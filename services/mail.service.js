aa6590af8d3090924833bdb6db22d0b20db39337
erankit011 <kumarankit97065@gmail.com>
2026-02-24 01:22:06 +0530

    update: fix SMTP server error

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,12 +1,43 @@
 const nodemailer = require("nodemailer");
 
+
+
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     secure: true,
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
+
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",   //host (more reliable)
+  port: 587,
+  secure: false,            
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
+
+
+// ✅ verify connection on server start
+const verifyTransporter = async () => {
+    try {
+        await transporter.verify();
+        console.log("✅ SMTP Server is ready to send emails");
+    } catch (error) {
+        console.error("❌ SMTP Connection Error:", {
+            message: error.message,
+            stack: error.stack,
+        });
+    }
+};
+
+verifyTransporter();
 
 module.exports = transporter;
